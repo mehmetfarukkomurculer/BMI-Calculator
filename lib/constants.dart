437aecc697cd5904c1aaf8a04dc72dbf48d79f56
mehmetfarukkomurculer <mfkomurculer@gmail.com>
2024-02-06 17:41:09 +0300

    implemented input page

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8d8e98),
+);
+
+const kHeightTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xff1d1e33);
+const kInactiveCardColour = Color(0xff111328);
+const kBottomContainerColour = Color(0xffeb1555);
